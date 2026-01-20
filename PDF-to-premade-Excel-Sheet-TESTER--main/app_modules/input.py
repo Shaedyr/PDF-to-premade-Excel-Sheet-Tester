df8fd09586f2c4f5ded02946bde33d93b57deeaf
Shaedyr <jonas95martensson@gmail.com>
2026-01-20 14:54:28 +0100

    Update input.py

--- PDF-to-premade-Excel-Sheet-TESTER--main/app_modules/input.py
+++ PDF-to-premade-Excel-Sheet-TESTER--main/app_modules/input.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from app_modules.Sheets.Sammendrag.Brreg_info_getter import search_brreg_live
+from app_modules.Sheets.Sammendrag.BRREG_info_getter import search_brreg_live
 
 
 def get_user_inputs():
@@ -77,4 +77,5 @@ def run():
     st.title("📄 Input-modul")
     st.write("Last opp PDF og søk etter selskap.")
     st.info("Denne modulen brukes av hovedsiden for å hente input-data.")
+
 
